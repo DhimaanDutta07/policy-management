5e869609b39b1c000d2bfa147b5328827c592079
DD <dhimaanmams.07@gmail.com>
2026-05-31 07:42:50 +0100

    Remove dist/ from git tracking, add to .gitignore - let Vercel build from source

--- policy-management-be/dist/utils/asyncTryCatch.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.asyncTryCatch = void 0;
-const asyncTryCatch = (fn) => {
-    return (req, res, next) => {
-        Promise.resolve(fn(req, res, next)).catch(next);
-    };
-};
-exports.asyncTryCatch = asyncTryCatch;
