27d723b475db118318df1ebb8488d24e2af17b59
DD <dhimaanmams.07@gmail.com>
2026-05-31 07:50:20 +0100

    Fix .gitignore path for dist/, remove dist/ from tracking again

--- policy-management-be/dist/src/schemas/commissionSchema.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.commissionUpdateSchema = exports.commissionSchema = void 0;
-const zod_1 = require("zod");
-exports.commissionSchema = zod_1.z.object({
-    name: zod_1.z.string().min(1, 'Name is required'),
-    description: zod_1.z.string().optional(),
-});
-exports.commissionUpdateSchema = exports.commissionSchema.partial();
